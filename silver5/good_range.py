1d1cc29367da2a4f177345fd3763988e40263638
sangwopa <79255978+sangwopa@users.noreply.github.com>
2021-09-25 22:29:55 +0900

    update 21.09.25

--- silver5/good_range.py
+++ silver5/good_range.py
@@ -15,14 +15,21 @@ def permutation(a, b):
             con.append(temp)
     return con
 
-# 해당되는 구간을 찾고, 구간안에서 카운트
-res = []
-for i in range(t - 1):
-    if (n > div[i]) & (n < div[i + 1]):
-        res = permutation(div[i] + 1, div[i + 1] - 1)
-        for u in range(len(res)):
-            if (n >= res[u][0]) & (n <= res[u][1]):
-                cnt += 1
+if len(div) == 1:
+    res = []
+    res = permutation(1, div[0] - 1)
+    for u in range(len(res)):
+        if (n >= res[u][0]) & (n <= res[u][1]):
+            cnt += 1
+else:
+    # 해당되는 구간을 찾고, 구간안에서 카운트
+    res = []
+    for i in range(t - 1):
+        if (n > div[i]) & (n < div[i + 1]):
+            res = permutation(div[i] + 1, div[i + 1] - 1)
+            for u in range(len(res)):
+                if (n >= res[u][0]) & (n <= res[u][1]):
+                    cnt += 1
 
 print(cnt)
 
